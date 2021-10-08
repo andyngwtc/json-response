509291747e1013ac5aa8994873b30b491a807fce
Andy Ng <andy.ng@wundermanthompson.com>
2021-10-08 16:44:07 +0100

    Added Composer Event Listener script

---
+++ scripts/ComposerEventListener.php
@@ -0,0 +1,18 @@
+<?php
+
+use Composer\Installer\PackageEvent;
+
+class ComposerEventListener
+{
+
+    public static function prePackageInstall(PackageEvent $event)
+    {
+        $composer = $event->getComposer();
+        var_dump("****************** Preparing to install packages ******************");
+    }
+
+    public static function postPackageInstall(PackageEvent $event)
+    {
+        var_dump("****************** Package install now going to generate lock file ******************");
+    }
+}
